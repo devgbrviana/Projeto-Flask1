17c3c035fa58c6a89796a5160b5650087f57ee29
Gabriel <gbr.vianas@gmail.com>
2025-03-19 23:45:16 -0300

    terceiro teste - retornando aluno por id

--- testeapp.py
+++ testeapp.py
@@ -41,3 +41,25 @@ class TestCrudAluno(unittest.TestCase):
         self.assertEqual(resposta.status_code, 200)
         alunos = resposta.json()
         self.assertGreater(len(alunos), 0)
+        
+
+    def test_003_retorna_aluno_por_id(self):
+        resposta = requests.post('http://localhost:5000/reseta')
+        resposta = requests.post('http://localhost:5000/alunos', json={
+            "nome": "Carlos",
+            "matricula": "54321",
+            "idade": 21,
+            "data_nascimento": "10/10/2000",
+            "nota_primeiro_semestre": 6.5,
+            "nota_segundo_semestre": 7.0,
+            "media_final": 6.75,
+            "turma_id": 1
+        })
+        aluno_criado = resposta.json()
+        aluno_id = aluno_criado['id']
+
+        resposta_get = requests.get(f'http://localhost:5000/alunos/{aluno_id}')
+        self.assertEqual(resposta_get.status_code, 200)
+        aluno_retorno = resposta_get.json()
+        self.assertEqual(aluno_retorno['id'], aluno_id)
+        self.assertEqual(aluno_retorno['nome'], 'Carlos')
