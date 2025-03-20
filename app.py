cb0b5796554c54d205b8a645cf4cf283f915a601
Gabriel <gbr.vianas@gmail.com>
2025-03-19 23:28:57 -0300

    POST professor

--- app.py
+++ app.py
@@ -18,3 +18,20 @@ def reseta():
 @app.route('/professores', methods=['GET'])
 def listar_professores():
     return jsonify(professores)
+
+
+@app.route('/professores', methods=['POST'])
+def criar_professor():
+    try:
+        data = request.get_json()
+        novo_professor = {
+            "id": len(professores) + 1,
+            "nome": data["nome"],
+            "especialidade": data["especialidade"],
+            "idade": data["idade"],
+            "info": data["info"]
+        }
+        professores.append(novo_professor)
+        return jsonify(novo_professor)
+    except Exception:
+        return jsonify({"erro": "Erro ao criar professor"})
