5c0a4ac3f982a6b67725d656fab269552c426e26
Gabriel <gbr.vianas@gmail.com>
2025-05-04 21:10:04 -0300

    model/professor/atualizado

--- professores/model.py
+++ professores/model.py
@@ -1,33 +1,92 @@
-professores = []
-id_professor_atual = 1
+from sqlalchemy import Column, String, Integer, Date
+from datetime import date, datetime
+from config import db
+
+class Professor(db.Model):
+    __tablename__ = "professores"
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    idade = db.Column(db.Integer)
+    data_nascimento = db.Column(db.Date)
+    disciplina = db.Column(db.String(100))
+    informacoes = db.Column(db.String(255))
+
+    turmas = db.relationship('Turma', backref='Professor', lazy=True)
+
+    def __init__(self, nome, data_nascimento, disciplina, informacoes):
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.idade = self.calcular_idade()
+        self.disciplina = disciplina
+        self.informacoes = informacoes
+
+    def calcular_idade(self):
+        hoje = date.today()
+        return hoje.year - self.data_nascimento.year - (
+            (hoje.month, hoje.day) < (self.data_nascimento.month, self.data_nascimento.day)
+        )
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'data_nascimento': self.data_nascimento.strftime("%d/%m/%Y"),
+            'nome': self.nome,
+            'idade': self.idade,
+            'disciplina': self.disciplina,
+            'informacoes': self.informacoes
+        }
 
 class ProfessorNaoEncontrado(Exception):
     pass
 
+# Funções de serviço
 def listar_professores():
-    return professores
+    professores = Professor.query.all()
+    return [professor.to_dict() for professor in professores]
 
-def professor_por_id(id_professor):
-    for professor in professores:
-        if professor['id'] == id_professor:
-            return professor
-    raise ProfessorNaoEncontrado()
+def professor_por_id(professor_id):
+    professor = Professor.query.get(professor_id)
+    if not professor:
+        raise ProfessorNaoEncontrado("Professor não encontrado.")
+    return professor.to_dict()
 
-def adicionar_professor(data):
-    global id_professor_atual
-    data['id'] = id_professor_atual
-    id_professor_atual += 1
-    professores.append(data)
+def adicionar_professor(dados):
+    try:
+        data_nascimento = datetime.strptime(dados["data_nascimento"], "%d/%m/%Y").date()
+    except ValueError:
+        raise ValueError("Formato de data inválido. Use o formato dd/mm/aaaa.")
+    
+    novo_professor = Professor(
+        nome=dados["nome"],
+        data_nascimento=data_nascimento,
+        disciplina=dados.get("disciplina"),
+        informacoes=dados.get("informacoes")
+    )
+    
+    db.session.add(novo_professor)
+    db.session.commit()
+    
+    return novo_professor.to_dict(), 201
 
-def atualizar_professor(id_professor, novos_dados):
-    professor = professor_por_id(id_professor)
-    professor.update(novos_dados)
+def atualizar_professor(professor_id, novos_dados):
+    professor = Professor.query.get(professor_id)
+    if not professor:
+        raise ProfessorNaoEncontrado("Professor não encontrado.")
 
-def excluir_professor(id_professor):
-    professor = professor_por_id(id_professor)
-    professores.remove(professor)
+    professor.nome = novos_dados["nome"]
+    professor.data_nascimento = datetime.strptime(novos_dados["data_nascimento"], "%d/%m/%Y").date()
+    professor.disciplina = novos_dados.get("disciplina", professor.disciplina)
+    professor.informacoes = novos_dados.get("informacoes", professor.informacoes)
+    professor.idade = professor.calcular_idade()
+    db.session.commit()
+    return {"message": "Professor atualizado com sucesso!"}
 
-def resetar_professores():
-    global professores, id_professor_atual
-    professores = []
-    id_professor_atual = 1
+def excluir_professor(professor_id):
+    professor = Professor.query.get(professor_id)
+    if not professor:
+        raise ProfessorNaoEncontrado("Professor não encontrado.")
+    
+    db.session.delete(professor)
+    db.session.commit()
+    return {"message": "Professor excluído com sucesso!"}
