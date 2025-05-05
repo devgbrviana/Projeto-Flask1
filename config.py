25fc75deb3ddf0edbc615b36dd7bfdc4af3fd0d1
Gabriel <gbr.vianas@gmail.com>
2025-05-04 21:23:09 -0300

    atualizando config

--- config.py
+++ config.py
@@ -1,3 +1,15 @@
-HOST = 'localhost'
-PORT = 5000
-DEBUG = True 
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+db = SQLAlchemy()
+
+def init_app(app):
+    app.config['HOST'] = '0.0.0.0'
+    app.config['PORT'] = 5000
+    app.config['DEBUG'] = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    
+    db.init_app(app)  # Inicializa SQLAlchemy com a app Flask
